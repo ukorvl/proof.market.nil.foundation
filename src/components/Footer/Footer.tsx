097751aa82c8c7dfdd48ec161bd3c5a417ccbe79
ukorvl <ukorotovskiy@gmail.com>
2022-09-22 18:55:58 +0400

    social links in footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import { ReactElement } from 'react';
 import { Container, Row, Col } from '@nilfoundation/react-components';
+import './Footer.scss';
+import { SocialLinks } from './SocialLinks';
 
 export const Footer = (): ReactElement =>
     <footer className="text-center">
         <div className="footer-below">
             <Container>
+                <Row>
+                    <Col>
+                        <SocialLinks />
+                    </Col>
+                </Row>
                 <Row>
                     <Col>
                         {`Copyright © =nil; Foundation ${ new Date().getFullYear() }`}
