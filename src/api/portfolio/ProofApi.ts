e6617c845f6e20ae89d8d5b12dd18032e3ff3498
ukorvl <ukorotovskiy@gmail.com>
2022-12-09 16:53:25 +0400

    include ie 11 in production

--- src/api/portfolio/ProofApi.ts
+++ src/api/portfolio/ProofApi.ts
@@ -22,8 +22,8 @@ export const getProofs = (): Promise<Proof> =>
             bindVars: {
                 '@relation': 'proof',
                 offset: 0,
-                count: 100,
+                count: 10,
             },
-            batchSize: 100,
+            batchSize: 10,
         })
         .then((x: any) => x.result);
