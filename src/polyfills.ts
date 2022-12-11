28f8aa38f7e6e0b2c98c5726db26aa5f8b766c40
ukorvl <ukorotovskiy@gmail.com>
2022-12-11 22:33:28 +0400

    include core-js/stable polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,4 +4,10 @@
  */
 
 import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+/**
+ * Injects core-js polyfills into bundle to provide old browsers support.
+ *
+ * @see {@link https://stackoverflow.com/questions/73194573/why-does-babel-in-create-react-app-not-polyfill-array-prototype-at-properly}.
+ */
+// eslint-disable-next-line
+const r = require('core-js/stable');
