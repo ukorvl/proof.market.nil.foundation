d81e1bfadceb710bcd008cee8feff7194d89363d
ukorvl <ukorotovskiy@gmail.com>
2022-12-16 15:58:38 +0400

    leave TODO for react-table@8 upgrade

--- src/components/common/ReactTable/ReactTableHeader.tsx
+++ src/components/common/ReactTable/ReactTableHeader.tsx
@@ -14,6 +14,7 @@ import styles from './ReactTable.module.scss';
  */
 type ReactTableHeaderProps<T extends Record<string, unknown>> = {
     column: ColumnInstance<T>;
+    // TODO - refactor after upgrading to react-table@v8
     onlySortBy?: 'asc' | 'desc';
 };
 
@@ -38,13 +39,14 @@ export const ReactTableHeader = <T extends Record<string, unknown>>({
         toggleSortBy();
     };
 
+    // TODO - refactor after upgrading to react-table@v8
     const shouldToggleSort = useMemo(() => {
         if (onlySortBy === undefined) {
             return false;
         }
 
         const toggleSortCondition = onlySortBy === 'asc' ? isSortedDesc : !isSortedDesc;
-        return isSorted && toggleSortCondition;
+        return isSorted && !!toggleSortCondition;
     }, [onlySortBy, isSortedDesc, isSorted]);
 
     useLayoutEffect(() => {
