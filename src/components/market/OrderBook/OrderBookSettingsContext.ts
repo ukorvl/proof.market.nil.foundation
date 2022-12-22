46138a00757a3b4ecd844f7afc010af335833851
ukorvl <ukorotovskiy@gmail.com>
2022-12-22 19:54:39 +0400

    OrderBookToolbar added

---
+++ src/components/market/OrderBook/OrderBookSettingsContext.ts
@@ -0,0 +1,22 @@
+/**
+ * @file React context.
+ * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
+ */
+
+import { createContext } from 'react';
+import { OrderBookPriceStep } from 'src/enums';
+
+/**
+ * Context type.
+ */
+type OrderBookSettingsContectModel = {
+    priceStep: OrderBookPriceStep;
+    setPriceStep: (s: OrderBookPriceStep) => void;
+};
+
+/**
+ * Order book settings context.
+ */
+export const OrderBookSettingsContext = createContext<OrderBookSettingsContectModel>(
+    {} as OrderBookSettingsContectModel,
+);
---
+++ src/enums/market/OrderBookPriceStep.ts
@@ -0,0 +1,15 @@
+/**
+ * @file Enum declaration.
+ * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
+ */
+
+/**
+ * Order book price step.
+ */
+export enum OrderBookPriceStep {
+    xs = '0.01',
+    sm = '0.1',
+    md = '1',
+    lg = '10',
+    xl = '100',
+}
