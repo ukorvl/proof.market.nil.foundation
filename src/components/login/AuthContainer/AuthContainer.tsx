ebe0e0c09438ec667ab7f74cb4bea5900aa13d0f
ukorvl <ukorotovskiy@gmail.com>
2022-12-23 12:28:07 +0400

    add back-link to auth container

--- src/components/login/AuthContainer/AuthContainer.tsx
+++ src/components/login/AuthContainer/AuthContainer.tsx
@@ -4,7 +4,10 @@
  */
 
 import { ReactElement, ReactNode } from 'react';
+import { Icon, useBreakpoints } from '@nilfoundation/react-components';
+import { Link } from 'react-router-dom';
 import { Copyright, FullScreenView } from 'src/components';
+import { Path } from 'src/routing';
 import styles from './AuthContainer.module.scss';
 
 /**
@@ -21,12 +24,24 @@ type AuthContainerProps = {
  * @returns React component.
  */
 export const AuthContainer = ({ children }: AuthContainerProps): ReactElement => {
+    const { md, lg } = useBreakpoints();
+
     return (
         <FullScreenView
             showFullScreen
             className={styles.loginContainer}
         >
             {children}
+            {md ||
+                (lg && (
+                    <Link
+                        className={styles.back}
+                        to={Path.root}
+                    >
+                        <Icon iconName="fa-solid fa-chevron-left" />
+                        Back to Market
+                    </Link>
+                ))}
             <div className={styles.copyright}>
                 <Copyright />
             </div>
