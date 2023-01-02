863961f8a6f9202621171440e2f4132ad88e7641
ukorvl <ukorotovskiy@gmail.com>
2023-01-02 22:47:26 +0400

    useSelectedCircuitId added

--- src/components/market/CircuitsList/CircuitsList.tsx
+++ src/components/market/CircuitsList/CircuitsList.tsx
@@ -3,18 +3,10 @@
  * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
  */
 
-import { ReactElement, useEffect } from 'react';
+import { ReactElement } from 'react';
 import { Spinner } from '@nilfoundation/react-components';
 import { dequal as deepEqual } from 'dequal';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { RouterParam } from 'src/enums';
-import {
-    selectCircuits,
-    selectCurrentCircuitId,
-    UpdateSelectedCircuitId,
-    useAppSelector,
-} from 'src/redux';
+import { selectCircuits, useAppSelector } from 'src/redux';
 import { CircuitsListTable } from './CircuitsListTable';
 import { DashboardCard } from '../../common';
 import styles from './CircuitsList.module.scss';
@@ -27,23 +19,6 @@ import styles from './CircuitsList.module.scss';
 export const CircuitsList = (): ReactElement => {
     const circuitsList = useAppSelector(selectCircuits, deepEqual);
     const loadingCircuits = useAppSelector(s => s.circuitsState.isLoading);
-    const selectedCircuitId = useAppSelector(selectCurrentCircuitId);
-    const circuitId = useParams()[RouterParam.circuitId];
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        if (selectedCircuitId === Number(circuitId)) {
-            return;
-        }
-
-        if (circuitId !== undefined) {
-            dispatch(UpdateSelectedCircuitId(Number(circuitId)));
-            return;
-        }
-
-        navigate(`${selectedCircuitId}`);
-    }, [circuitId, dispatch, selectedCircuitId, navigate]);
 
     return (
         <DashboardCard>
