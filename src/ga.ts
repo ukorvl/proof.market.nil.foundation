00a118145aa3ccd5331ea0738a9bd75cb01c6cd7
ukorvl <ukorotovskiy@gmail.com>
2023-01-04 15:31:46 +0400

    use react-ga4

--- src/ga.ts
+++ src/ga.ts
@@ -3,12 +3,14 @@
  * @copyright Yury Korotovskikh 2023 <u.korotovskiy@nil.foundation>
  */
 
-import { initialize, InitializeOptions } from 'react-ga';
+import ReactGa from 'react-ga4';
 
 /**
  * Ga initialize options.
  */
-const gaInitOptions: InitializeOptions = {};
+const gaInitOptions = {
+    testMode: process.env.NODE_ENV !== 'production',
+};
 
 /**
  * Configures ga for the app. Should be called before any other tracking functions will record any data.
@@ -16,13 +18,13 @@ const gaInitOptions: InitializeOptions = {};
 export default function configureGA(): void {
     const measurementId = process.env.REACT_APP_GA_TRACKING_ID;
 
-    if (process.env.NODE_ENV !== 'production') {
-        return;
-    }
+    // if (process.env.NODE_ENV !== 'production') {
+    //     return;
+    // }
 
     if (!measurementId) {
         return;
     }
 
-    initialize(measurementId, gaInitOptions);
+    ReactGa.initialize(measurementId, gaInitOptions);
 }
