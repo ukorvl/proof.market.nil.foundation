641dba7ea7f86366fffb6bf5c9ba4a14cdd808b7
ukorvl <ukorotovskiy@gmail.com>
2023-01-18 12:27:14 +0400

    add docs to mobile menu #8

--- src/components/common/MobileMenu/MobileMenu.tsx
+++ src/components/common/MobileMenu/MobileMenu.tsx
@@ -5,7 +5,7 @@
 
 import { ReactElement } from 'react';
 import { Dropdown } from '@nilfoundation/react-components';
-import { navigationLinks } from '../../../constants';
+import { documentationUrl, navigationLinks } from 'src/constants';
 import styles from './MobileMenu.module.scss';
 
 /**
@@ -30,6 +30,7 @@ export const MobileMenu = (): ReactElement => {
                         {title}
                     </Dropdown.Item>
                 ))}
+                <Dropdown.Item href={documentationUrl}>Docs</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
