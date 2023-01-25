882e1e9cea9e642c72ee745b9cef8f96cf37e8d6
ukorvl <ukorotovskiy@gmail.com>
2023-01-25 22:10:52 +0400

    refactor proof api #8

--- src/api/portfolio/ProofApi.ts
+++ src/api/portfolio/ProofApi.ts
@@ -4,59 +4,21 @@
  */
 
 import { Proof } from '../../models';
-import { createBearerHttpClient } from '../common';
+import { apiBaseUrl, createBearerHttpClient } from '../common';
 
-const db = process.env.REACT_APP_DBMS_DEFAULT_DATABASE;
-const httpFetcher = createBearerHttpClient(`_db/${db}/_api/`);
-
-/**
- * Get proofs.
- *
- * @param currentUser Current user.
- * @returns Proofs.
- */
-export const getProofs = (currentUser: string): Promise<Proof> =>
-    httpFetcher
-        .post('cursor', {
-            query: `
-                LET bidsByUser = (
-                    FOR doc in bid
-                    FILTER doc.status == 'completed'
-                    FILTER doc.sender == '${currentUser}'
-                    RETURN doc
-                )
-
-                FOR x IN @@relation
-                    LET contained = (
-                        FOR doc in bidsByUser
-                        FILTER x.bid_key == doc._key
-                        RETURN doc
-                    )
-                    FILTER LENGTH(contained) > 0
-                    RETURN UNSET(x, 'proof')
-            `,
-            bindVars: {
-                '@relation': 'proof',
-            },
-        })
-        .then((x: any) => x.result);
+const httpFetcher = createBearerHttpClient(`${apiBaseUrl}/proof`);
 
 /**
- * Get proof by its id.
+ * Get current user proofs.
  *
- * @param proofId Proof id.
  * @returns Proofs.
  */
-export const getProofById = (proofId: string): Promise<Proof> =>
-    httpFetcher
-        .post('cursor', {
-            query: `
-                FOR x IN @@relation
-                    FILTER x.id == '${proofId}'
-                    RETURN x
-            `,
-            bindVars: {
-                '@relation': 'proof',
-            },
-        })
-        .then((x: any) => x.result);
+export const getProofs = (): Promise<Proof[]> => httpFetcher.get('/owner');
+
+/**
+ * Get proof by key.
+ *
+ * @param proofKey Proof id.
+ * @returns Proofs.
+ */
+export const getProofById = (proofKey: string): Promise<Proof> => httpFetcher.get(`/${proofKey}`);
