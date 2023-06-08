82188a7417129f2f6dd4bd3bd0c07e17b652cd1f
ukorvl <ukorotovskiy@gmail.com>
2023-06-08 18:22:13 +0400

    remove jwt revalidation #105

--- src/redux/login/sagas/AuthSaga.ts
+++ src/redux/login/sagas/AuthSaga.ts
@@ -1,22 +1,17 @@
 /**
  * @file Redux saga.
+ * @todo Add token renewal after implementing on backend.
  * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
  */
 
-import { call, cancel, delay, fork, put, select, take, takeLatest } from 'redux-saga/effects';
+import { call, fork, put } from 'redux-saga/effects';
 import type { SagaIterator } from '@redux-saga/core';
 import type { GoogleUserinfo } from '@/models';
-import { getGoogleProfileInfo, renewJwt } from '@/api';
-import { getItemFromLocalStorage, setItemIntoLocalStorage } from '@/packages/LocalStorage';
-import { calculateRenewJwtTimeGap, clearAuthLocalStorageState, getUserFromJwt } from '@/utils';
+import { getGoogleProfileInfo } from '@/api';
+import { getItemFromLocalStorage } from '@/packages/LocalStorage';
+import { clearAuthLocalStorageState, getUserFromJwt } from '@/utils';
 import { AuthType } from '@/enums';
-import {
-    SetJwtRevalidateTimeout,
-    UpdateGoogleUserInfo,
-    UpdateIsAuthorized,
-    UpdateUserName,
-} from '../actions';
-import { selectUserName } from '../selectors';
+import { UpdateGoogleUserInfo, UpdateIsAuthorized, UpdateUserName } from '../actions';
 
 /**
  * Auth main saga.
@@ -25,7 +20,7 @@ import { selectUserName } from '../selectors';
  */
 export function* AuthSaga(): SagaIterator<void> {
     yield fork(TryGetUserFromLocalStorageTokenSaga);
-    yield takeLatest(SetJwtRevalidateTimeout, RenewJwtSaga);
+    //yield takeLatest(SetJwtRevalidateTimeout, RenewJwtSaga);
 }
 
 /**
@@ -41,10 +36,10 @@ function* TryGetUserFromLocalStorageTokenSaga(): SagaIterator<void> {
         return;
     }
 
-    yield put(UpdateIsAuthorized(true));
-
     switch (authType) {
         case AuthType.google: {
+            yield put(UpdateIsAuthorized(true));
+
             try {
                 const response: GoogleUserinfo = yield call(getGoogleProfileInfo, token);
                 yield put(UpdateUserName(response.name));
@@ -63,9 +58,10 @@ function* TryGetUserFromLocalStorageTokenSaga(): SagaIterator<void> {
             const user = getUserFromJwt(token);
             if (user) {
                 yield put(UpdateUserName(user));
+                //yield put(UpdateIsAuthorized(true));
 
-                const timeout = calculateRenewJwtTimeGap(token);
-                yield put(SetJwtRevalidateTimeout(timeout));
+                //const timeout = calculateRenewJwtTimeGap(token);
+                //yield put(SetJwtRevalidateTimeout(timeout));
             }
             break;
         }
@@ -75,53 +71,53 @@ function* TryGetUserFromLocalStorageTokenSaga(): SagaIterator<void> {
     }
 }
 
-/**
- * Renews jwt token to prevent auto-logout from dbms instance.
- *
- * @param {ReturnType<typeof SetJwtRevalidateTimeout>} params .
- * @yields
- */
-function* RenewJwtSaga({
-    payload: timeout,
-}: ReturnType<typeof SetJwtRevalidateTimeout>): SagaIterator<void> {
-    const task = yield fork(processRenewJwt, timeout);
+// /**
+//  * Renews jwt token to prevent auto-logout from dbms instance.
+//  *
+//  * @param {ReturnType<typeof SetJwtRevalidateTimeout>} params .
+//  * @yields
+//  */
+// function* RenewJwtSaga({
+//     payload: timeout,
+// }: ReturnType<typeof SetJwtRevalidateTimeout>): SagaIterator<void> {
+//     const task = yield fork(processRenewJwt, timeout);
 
-    while (true) {
-        const { payload: user } = yield take(UpdateUserName);
-        if (!user) {
-            yield cancel(task);
-        }
-    }
-}
+//     while (true) {
+//         const { payload: user } = yield take(UpdateUserName);
+//         if (!user) {
+//             yield cancel(task);
+//         }
+//     }
+// }
 
-/**
- * Renew jwt process.
- *
- * @param timeout Revalidation timeout.
- * @yields
- */
-function* processRenewJwt(timeout: number): SagaIterator<void> {
-    yield delay(timeout);
+// /**
+//  * Renew jwt process.
+//  *
+//  * @param timeout Revalidation timeout.
+//  * @yields
+//  */
+// function* processRenewJwt(timeout: number): SagaIterator<void> {
+//     yield delay(timeout);
 
-    try {
-        const currentUser = yield select(selectUserName);
+//     try {
+//         const currentUser = yield select(selectUserName);
 
-        const result: Awaited<ReturnType<typeof renewJwt>> = yield call(renewJwt, currentUser);
-        const { jwt } = result;
+//         const result: Awaited<ReturnType<typeof renewJwt>> = yield call(renewJwt, currentUser);
+//         const { jwt } = result;
 
-        if (!jwt) {
-            return;
-        }
+//         if (!jwt) {
+//             return;
+//         }
 
-        const user = getUserFromJwt(jwt);
-        const timeout = calculateRenewJwtTimeGap(jwt);
+//         const user = getUserFromJwt(jwt);
+//         const timeout = calculateRenewJwtTimeGap(jwt);
 
-        if (user === currentUser) {
-            setItemIntoLocalStorage('userToken', jwt);
+//         if (user === currentUser) {
+//             setItemIntoLocalStorage('userToken', jwt);
 
-            yield put(SetJwtRevalidateTimeout(timeout));
-        }
-    } catch (e) {
-        // Do nothing
-    }
-}
+//             yield put(SetJwtRevalidateTimeout(timeout));
+//         }
+//     } catch (e) {
+//         // Do nothing
+//     }
+// }
